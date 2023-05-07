2bf1f045159e8bc432d76edc6828c8ad90506f64
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-07 12:53:40 -0400

    add show index and html file with arguments error in index (which was previously working

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -4,4 +4,12 @@ class LogsController < ApplicationController
     @logs = Log.all
     render :index
   end
+
+
+  def show
+    @log = Log.find_by(id: params[:id])
+    index :show
+  end 
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :logs 
 
   get "/logs" => "logs#index"
+  get "/logs/:id" => "logs#show"
+  
 end

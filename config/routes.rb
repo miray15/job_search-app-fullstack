17272e82c7b97c0d88a4507b759b0b8fe5702820
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-15 17:35:09 -0500

    add job adjust links on webpages to work

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
   get "/logs" => "logs#new"
   get "/logs" => "logs#create"
 
+
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+  
 end
